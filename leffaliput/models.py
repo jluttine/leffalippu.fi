8a5b958eb5e0f74e8a1022a1a9293fc01064755f
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2013-03-08 03:35:05 +0200

    First sketch of the models

--- leffaliput/models.py
+++ leffaliput/models.py
@@ -20,4 +20,99 @@ Models for `leffaliput`.
 
 from django.db import models
 
-# Create your models here.
+class OrderManager(models.Manager):
+
+    # When a status of an order changes, remember to update the ticket
+    # too! If the order is expired or cancelled, the ticket must be
+    # freed for new orders.
+
+    def create_order(self, email):
+        # Send email with payment instructions
+        pass
+
+    def check_status(self):
+        # Go through orders and check whether some have expired or
+        # been paid..
+        pass
+
+
+class Order(model.Model):
+    """
+    Class for handling orders of tickets.
+
+    When an order is created, it is open until it either expires, is
+    cancelled or is paid. Tickets are kept reserved for the order
+    until it either expires or is cancelled.
+    """
+
+    """ Id of the order used in URL """
+    key = models.CharField(max_length=20, primary_key=True)
+    """ Timestamp of the order """
+    date = models.DateTimeField(auto_now_add=True)
+    """ Email address of the customer """
+    email = models.EmailField()
+    # For bitcoin payment
+    public_address 
+    private_key
+    
+    # Status: open -> expired/cancelled/paid
+    OPEN = 'O'
+    PAID = 'P'
+    CANCELLED = 'C'
+    EXPIRED = 'E'
+    STATUS_CHOICES = (
+        (OPEN, 'Open'),
+        (PAID, 'Paid'),
+        (CANCELLED, 'Cancelled'),
+        (EXPIRED, 'Expired'),
+    )
+    """ Current status of the order """
+    status = models.CharField(max_length=1,
+                              choices=STATUS_CHOICES,
+                              default=OPEN)
+
+    # Use custom manager?
+    objects = OrderManager()
+
+    def cancel(self):
+        pass
+
+    def expire(self):
+        pass
+
+    def pay(self):
+        pass
+
+class TicketType(model.Model):
+    """
+    A class for types of tickets.
+    """
+    
+    """ Short name of the ticket """
+    name = models.CharField(primary_key=True)
+    
+    """ Long description of the ticket and how to use """
+    description = models.TextField()
+    
+    """ Price of the ticket in cents """
+    price = models.PositiveIntegerField()
+
+class Ticket(model.Model):
+    """
+    Class for a ticket.
+
+    A ticket may be reserved/buyed or available.
+    """
+
+    """ The order for which this ticket is reserved or used (if any) """
+    order = models.ForeignKey(Order, 
+                              blank=True,
+                              null=True,
+                              on_delete=models.SET_NULL,
+                              related_name='tickets')
+
+    """ Type of the ticket """
+    tyyppi = models.ForeignKey(TicketType)
+
+    """ Serial number or other identification code """
+    number = models.CharField()
